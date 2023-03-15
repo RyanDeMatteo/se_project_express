f84b7c611c5cabe939137c1933dba777811d4e9c
RyanDeMatteo <110950877+RyanDeMatteo@users.noreply.github.com>
2023-03-14 21:49:36 -0400

    lint roll refactor 5

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,18 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
+const { ERROR_CODES } = require("../utils/errors");
+
+const handleAuthenticationError = (res) => {
+  res
+    .status(ERROR_CODES.Unauthorized)
+    .send({ message: "Authorization required" });
+};
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer")) {
-    return res.status(401).send({ message: "Authorization required" });
+    handleAuthenticationError(res);
   }
   const token = authorization.replace("Bearer", "");
   let payload;
@@ -13,7 +20,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res.status(401).send({ message: "Authorization Required" });
+    handleAuthenticationError(res);
   }
 
   req.user = payload;
